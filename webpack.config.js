bd2efb61a67e4ca4436cda4bcae70094d4b7aa98
dengjiayao <dnxbf321@gmail.com>
2017-03-03 17:22:18 +0800

    更新至 4.3

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,34 @@
  * 默认配置请查看 wcx/script/webpack-conf
  * 配置项请参考：https://webpack.js.org/configuration/#options
  * 可使用 process.env.NODE_ENV 区分 development、experiment、production 环境
- * @type {Object}
  */
-module.exports = {
-  module: {
-    rules: []
+
+/**
+ * webpack 自定义配置，与默认配置合并
+ * @param  {object} rcObject 配置项 {babel:{}, eslint:{}, postcss:{}}
+ * @return {function}
+ */
+module.exports = function(rcObject) {
+  return {
+    module: {
+      rules: [
+        {
+          test: /\.vue$/,
+          use: [
+            {
+              loader: 'vue-loader',
+              options: {
+                loaders: {
+                  js: 'babel-loader?' + JSON.stringify(rcObject.babel) + '!eslint-loader?' + JSON.stringify(rcObject.eslint)
+                },
+                postcss: {
+                  plugins: rcObject.postcss
+                }
+              }
+            }
+          ]
+        }
+      ]
+    }
   }
 }
