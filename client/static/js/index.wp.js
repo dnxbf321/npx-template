faf777f6a58e654993a288361b46c5f8d5a1645e
dengjiayao <dnxbf321@gmail.com>
2017-09-06 13:20:55 +0800

    v 5.0.0

--- client/static/js/index.wp.js
+++ client/static/js/index.wp.js
@@ -1,1 +1,16 @@
+import '../css/_test.css'
+
 console.log(client.publicPath)
+
+let arr = [1, 2, 3]
+let copyArr = [...arr]
+console.log(copyArr)
+
+let filed = {
+  a: 1,
+  b: 2
+}
+let obj = {
+  ...filed
+}
+console.log(obj)
