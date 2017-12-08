628d6addaf1ba2f3fcfba03b0a4c4b90552c0470
dengjiayao <dnxbf321@gmail.com>
2017-12-08 15:46:40 +0800

    环境变量是数组

--- client/static/js/index.wp.js
+++ client/static/js/index.wp.js
@@ -1,6 +1,9 @@
 import '../css/_test.css'
 
+console.log(client, typeof client)
 console.log(client.publicPath)
+console.log(debug, typeof debug)
+console.log(array1)
 
 let arr = [1, 2, 3]
 let copyArr = [...arr]
